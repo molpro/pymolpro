cf984487efe1e6971ff17d039b990768b7197463
Marat <sibaev.marat@gmail.com>
2020-01-15 16:48:20 +0000

    update pysjef

--- pysjef_molpro/__init__.py
+++ pysjef_molpro/__init__.py
@@ -3,10 +3,9 @@ from pysjef import *
 from pysjef_molpro.project import MProject, no_errors
 import pysjef_molpro.node_xml as node_xml
 
-pysjef.PROJECT_FACTORY["molpro"] = MProject
-pysjef.PROJECT_FACTORY_DEFAULT_SUFFIX = 'molpro'
+Settings.set('project_default_suffix', 'molpro')
+pysjef.project_factory.PROJECT_FACTORY["molpro"] = MProject
 
-pysjef.RootXml.TAG_TO_NAME['molpro'] = node_xml.tag_to_name
+RootXml.TAG_TO_NAME['molpro'] = node_xml.tag_to_name
 RootXml.SPECIAL_NODES['molpro'] = {'property': node_xml.PropertyXml,
                                    'plot': node_xml.PlotXml}
-pysjef.RootXml.DEFAULT_SUFFIX = 'molpro'
