3dc34255884b3268a2988f1ca32cb9039030efc1
Marat Sibaev <sibaev.marat@gmail.com>
2020-04-08 11:38:11 +0100

    add version

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 
 
+def read_version():
+    version = "none-0.0.0"
+    with open("pysjef_molpro/_version.py", "r") as f:
+        for line in f.readlines():
+            if "__version__" in line:
+                version = line.split("=")[-1].strip().strip(' "').strip("'")
+    return version
+
+
 setup(
     name="pysjef_molpro",
+    version=read_version(),
     packages=find_packages(),
     license="MIT",
-    install_requires=["pysjef", "numpy>=1.12", "regex"],
+    install_requires=["pysjef>=0.0.1", "numpy>=1.12", "regex"],
 )
