605781b6dca723c82b03e0c88b247e46f7504c11
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-10-13 15:27:14 +0100

    Refresh documentation framework

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,9 @@ release = pymolpro.__version__
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # 'sphinx.ext.autodoc'
+    ]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -34,5 +36,5 @@ del frozen_locals
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
