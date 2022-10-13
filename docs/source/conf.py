f1f53d5c2168ddad20c61d0041550c3a27c6aed6
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2022-10-13 09:12:45 +0100

    Refresh documentation framework

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,11 +23,11 @@ import pip
 # pip.main(['install',"--no-deps","--force-reinstall",BASE_DIR])
 # pip.main(['install'])
 # import conda.cli
-# import subprocess
+import subprocess
 # subprocess.check_call(['conda','install',"-c","conda-forge",'-y','versioneer'])
 # subprocess.check_call(['conda','list'])
 # import versioneer
-# subprocess.check_call([sys.executable,"-m","pip","install","--no-deps","--force-reinstall",Path(__file__).parent.parent.parent])
+subprocess.check_call([sys.executable,"-m","pip","install","--no-deps","--force-reinstall",Path(__file__).parent.parent.parent])
 import pymolpro
 release = pymolpro.__version__
 
