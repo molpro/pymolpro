d36440e0737f075fcfc6255dbf2473e9875932c2
GitHub Action <action@github.com>
2022-10-13 15:04:56 +0000

    Add changes for 8232dcfcfb0c6c65d1744206b4ee9aa459356846

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3+1.g6076f16',
+    VERSION: '0.0.3+2.g8232dcf',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
