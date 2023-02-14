202774efd612acd8f3fad1831c6d9dc703c24d49
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-02-14 19:05:31 +0000

    Start development of orbital tuple support

--- pymolpro/__init__.py
+++ pymolpro/__init__.py
@@ -3,6 +3,7 @@ from pysjef import *
 from pymolpro.project import no_errors, element_to_dict
 import pymolpro.node_xml as node_xml
 from pymolpro.orbital import Orbital
+from pymolpro.tuple import Tuple, Single, Pair
 from . import _version
 
 Settings.set('project_default_suffix', 'molpro')
@@ -13,3 +14,26 @@ RootXml.SPECIAL_NODES['molpro'] = {'property': node_xml.PropertyXml,
                                    'plot': node_xml.PlotXml}
 
 __version__ = _version.get_versions()['version']
+
+
+def xpath(element, query):
+    """
+    Run xpath search on an element in the job xml, with support for default namespace
+
+    :param element: The root element for the search
+    :param query: Any xpath search expression supported by lxml.etree.Element
+    :return: list of etree.Element objects or of strings, depending on whether the xpath expression results in an attribute
+    """
+    from lxml import etree
+    from io import StringIO
+    default_ns_name = '__default__'
+    ns = {k if k is not None else default_ns_name: v for k, v in element.getroottree().getroot().nsmap.items()}
+    # print("ns:",ns)
+    import re
+    queryns = re.sub(r'(::|/|^)([_a-zA-Z][-._a-zA-Z0-9]*)(?=/|$|\[)', r'\1' + default_ns_name + r':\2', query)
+    # print("queryns",queryns)
+    try:
+        return element.xpath(queryns, namespaces=ns)
+    except Exception as e:
+        print("xpath query failed:", e)
+        print("query =", query, ", queryns =", queryns, ", namespaces =", ns)
---
+++ pymolpro/test_pair.py
@@ -0,0 +1,45 @@
+import unittest
+
+import lxml.etree
+
+import pymolpro
+import math
+import numpy as np
+from lxml import etree
+
+
+class TestPair(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        root = lxml.etree.fromstring('''<?xml version="1.0"?>
+<molpro xmlns="http://www.molpro.net/schema/molpro-output"
+  xmlns:xsd="http://www.w3.org/1999/XMLSchema"
+  xmlns:cml="http://www.xml-cml.org/schema"
+  xmlns:stm="http://www.xml-cml.org/schema"
+  xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  <orbitals>
+  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">1.0 2.0 </orbital>
+  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">3.0 4.0 </orbital>
+  </orbitals>
+  <pair orbital1="1.1" orbital2="-2.1" energy="1.2345"/>
+  <pair orbital1="1.1" orbital2="2.1" energy="0.2345"/>
+  </molpro>''')
+        print(etree.tostring(root, pretty_print=True))
+        cls.pair = pymolpro.Pair(pymolpro.xpath(root, "//pair")[-2])
+        cls.root = root
+
+    def test_construction(self):
+        pair = self.pair
+        self.assertEqual(len(pair.orbitals), 2)  # add assertion here
+        self.assertEqual(pair.orbitals[0].ID, '1.1')  # add assertion here
+        self.assertEqual(pair.orbitals[1].ID, '2.1')  # add assertion here
+        self.assertEqual(pair.spins,[1,-1])
+        self.assertAlmostEqual(float(pair.node.get('energy')),1.2345)
+        self.assertAlmostEqual(pair.energy,1.2345)
+        print(self.pair.data())
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
