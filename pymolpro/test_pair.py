4c799e406efc2c8304df69a811a3c6fee0e317bf
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-02-16 17:42:50 +0000

    more robust parsing of orbitals for pair (#22)

--- pymolpro/test_pair.py
+++ pymolpro/test_pair.py
@@ -17,26 +17,64 @@ class TestPair(unittest.TestCase):
   xmlns:cml="http://www.xml-cml.org/schema"
   xmlns:stm="http://www.xml-cml.org/schema"
   xmlns:xhtml="http://www.w3.org/1999/xhtml">
+  <jobstep>
+  <pair orbital1="1.1" orbital2="-2.1" energy="3.2345"/>
+  <pair orbital1="1.1" orbital2="2.1" energy="4.2345"/>
+  </jobstep>
+  <molecule>
   <orbitals>
-  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">1.0 2.0 </orbital>
-  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">3.0 4.0 </orbital>
+  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-1.0 -2.0</orbital>
+  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-3.0 -4.0</orbital>
   </orbitals>
+  </molecule>
+  <molecule>
+  <orbitals>
+  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">1.0 2.0</orbital>
+  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">3.0 4.0</orbital>
+  </orbitals>
+  </molecule>
+  <jobstep>
   <pair orbital1="1.1" orbital2="-2.1" energy="1.2345"/>
   <pair orbital1="1.1" orbital2="2.1" energy="0.2345"/>
+  </jobstep>
+  <molecule>
+  <orbitals>
+  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-1.0 -2.0</orbital>
+  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-3.0 -4.0</orbital>
+  </orbitals>
+  </molecule>
+  <molecule>
+  <orbitals>
+  <orbital occupation="2.0" ID="1.1" moments="0.0 0.0 -1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-11.0 -12.0</orbital>
+  <orbital occupation="2.0" ID="2.1" moments="0.0 0.0 1.0 2.0 2.0 2.0 0.0 0.0 0.0 0.0">-13.0 -14.0</orbital>
+  </orbitals>
+  </molecule>
   </molpro>''')
-        print(etree.tostring(root, pretty_print=True))
+        # print(etree.tostring(root, pretty_print=True))
         cls.pair = pymolpro.Pair(pymolpro.xpath(root, "//pair")[-2])
         cls.root = root
 
     def test_construction(self):
-        pair = self.pair
+        pair = pymolpro.Pair(pymolpro.xpath(self.root, "//pair")[-2])
         self.assertEqual(len(pair.orbitals), 2)  # add assertion here
         self.assertEqual(pair.orbitals[0].ID, '1.1')  # add assertion here
         self.assertEqual(pair.orbitals[1].ID, '2.1')  # add assertion here
-        self.assertEqual(pair.spins,[1,-1])
-        self.assertAlmostEqual(float(pair.node.get('energy')),1.2345)
-        self.assertAlmostEqual(pair.energy,1.2345)
-        print(self.pair.data())
+        self.assertEqual(pair.spins, [1, -1])
+        self.assertAlmostEqual(float(pair.node.get('energy')), 1.2345)
+        self.assertAlmostEqual(pair.energy, 1.2345)
+        self.assertEqual(pair.orbitals[0].node.text, "1.0 2.0")
+        self.assertEqual(len(pair), 2)
+
+    def test_construction_forward_orbitals(self):
+        pair = pymolpro.Pair(pymolpro.xpath(self.root, "//pair")[1])
+        self.assertEqual(len(pair.orbitals), 2)  # add assertion here
+        self.assertEqual(pair.orbitals[0].ID, '1.1')  # add assertion here
+        self.assertEqual(pair.orbitals[1].ID, '2.1')  # add assertion here
+        self.assertEqual(pair.spins, [1, 1])
+        self.assertAlmostEqual(float(pair.node.get('energy')), 4.2345)
+        self.assertAlmostEqual(pair.energy, 4.2345)
+        self.assertEqual(pair.orbitals[0].node.text, "-1.0 -2.0")
+        self.assertEqual(len(pair), 2)
 
 
 if __name__ == '__main__':
