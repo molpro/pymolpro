8d1f8fbf11a7b0a064199074cdd1512084d5f477
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-02-16 08:18:37 +0000

    doc

--- pymolpro/tuple.py
+++ pymolpro/tuple.py
@@ -6,16 +6,18 @@ from pymolpro.orbital import Orbital
 class Tuple:
     """
     Container for a tuple of orbitals
+
+    :param node: Node holding a correlation single or pair descriptor
+    :type node: lxml.etree.Element
     """
 
     def __init__(self, node):
         """
         Initialise from a node on a Molpro output xml tree
 
-        :param node: lxml.etree.Element holding a correlation single or pair descriptor
         """
         self.node = node
-        self.spins = [1]
+        self.spins = [1] #: 1(alpha) or -1(beta) for each orbitala
         orbital1 = node.get("orbital1") if node.get("orbital") is None else node.get("orbital")
         if orbital1[0] == '-':
             self.spins[0] = -1
@@ -38,33 +40,9 @@ class Tuple:
                 orbital3 = orbital3[1:]
         # TODO UHF case
         self.orbitals = [Orbital(node) for node in xpath(self.node,
-                                                         '/molpro//orbitals/orbital[@ID="' + orbital1 + '" or @ID="' + orbital2 + '" or @ID="' + orbital3 + '"]')]
+                                                         '/molpro//orbitals/orbital[@ID="' + orbital1 + '" or @ID="' + orbital2 + '" or @ID="' + orbital3 + '"]')] #: Orbital objects forming the tuple
         if orbital2 == orbital1: self.orbitals.append(self.orbitals[0])
-        self.energy = None if self.node.get('energy') is None else float(self.node.get('energy'))
-
-    def data(self, grid=2):
-        """
-        Construct a set of floating point data describing the orbital tuple together with the tuple energy
-
-        :param grid: Number of orbital grid points in each direction
-        :return: flat numpy array
-        """
-        result = np.array([], dtype=float)
-        for particle in range(len(self.spins)):
-            result = np.append(result, self.spins[particle])
-            orbital = self.orbitals[particle]
-            orbital_grid = np.reshape(orbital.grid(grid), (-1, 4))[:, :3]
-            print("orbital_grid: ", orbital_grid)
-            result = np.append(result, orbital_grid)
-            for other_particle in range(particle + 1, len(self.spins)):
-                other_orbital = self.orbitals[other_particle]
-                other_orbital_grid = np.reshape(other_orbital.grid(grid), (-1, 4))[:, :3]
-                for point in orbital_grid:
-                    for other_point in other_orbital_grid:
-                        dist = np.linalg.norm(point - other_point)
-                        result = np.append(result, 1 / dist)
-        return [result, self.energy]
-
+        self.energy = None if self.node.get('energy') is None else float(self.node.get('energy')) #: Correlation energy contribution from a Single or Pair
 
 class Pair(Tuple):
     """
