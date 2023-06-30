e0c62ec8b219007003af17fdce08f39745cab2eb
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2023-06-30 13:58:22 +0200

    Add violin plots

--- pymolpro/project.py
+++ pymolpro/project.py
@@ -118,7 +118,7 @@ class Project(pysjef.project.Project):
             raise FileNotFoundError("Cannot open project "+name)
         if geometry != "":  # construct input
             __method = method.lower()
-            if __method[-2:] != 'hf' and __method[-2:] != 'ks' and 'ks,' not in __method and 'ks ' not in __method:
+            if __method[-2:] != 'hf' and __method[1:3] != 'hf' and __method[-2:] != 'ks' and 'ks,' not in __method and 'ks ' not in __method:
                 if __method[:2] == 'df':
                     if __method[:4] == 'df-u':
                         __method = 'df-uhf; df-'+__method[4:]
