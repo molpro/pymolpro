f6515954950fcb131bd59eea032c1c44beaea9a4
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2024-02-26 07:26:52 +0000

    Fix run_local_molpro

--- pymolpro/project.py
+++ pymolpro/project.py
@@ -484,14 +484,12 @@ basis={basis}
                     return result
                 else:
                     result = result + string[i]
-        command_ = (self.backend_get('local', 'run_command'))
-        # print((['/bin/sh'] if shutil.which('/bin/sh') else []) +
-        # re.sub('  *',' ',re.sub('{.*?}','',command_)).split(' ') +
-        # options)
-        return subprocess.run((['/bin/sh'] if shutil.which('/bin/sh') else []) +
-        # [ shutil.which(command_)] +
-        re.sub('mpiexec','mpiexec -n 1', re.sub('  *',' ', re.sub('{.*?}','',command_))).split(' ') +
-        options, capture_output=True)
+        command_ = (self.backend_get('local', 'run_command')).strip(' ')
+        first_command = get_first(command_)
+        command_ = re.sub(r'^'+first_command, shutil.which(first_command), command_)
+        command_ = re.sub('mpiexec', 'mpiexec -n 1', re.sub('  *', ' ', re.sub('{.*?}', '', command_))).strip(' ')
+        split_options = (['/bin/sh'] if shutil.which('/bin/sh') else []) + command_.split(' ') + options
+        return subprocess.run(split_options, capture_output=True)
 
     def registry(self, set=None):
         """
