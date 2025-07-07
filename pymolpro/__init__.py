833b733994137096139b6ac5452e1e0596fff7af
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-07-07 10:53:41 +0100

    Implement ASE calculator

--- pymolpro/__init__.py
+++ pymolpro/__init__.py
@@ -7,8 +7,9 @@ from pymolpro.tuple import Tuple, Single, Pair
 import pymolpro.database
 from ._version import __version__, __version_tuple__
 from pymolpro.geometry import xyz_to_zmat
+from pymolpro.ase import ASEMolpro
 
-__all__ = ['Project', 'Orbital', 'Pair', 'Single', 'Tuple', 'no_errors', 'element_to_dict', 'Database', 'xyz_to_zmat']
+__all__ = ['Project', 'Orbital', 'Pair', 'Single', 'Tuple', 'no_errors', 'element_to_dict', 'Database', 'xyz_to_zmat', 'ASEMolpro']
 
 Settings.set('project_default_suffix', 'molpro')
 pysjef.project_factory.PROJECT_FACTORY["molpro"] = project.Project
