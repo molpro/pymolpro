87504da3c7a8766915c1da0458eeb70a0fd23cad
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2025-08-14 22:49:31 +0100

    Free functions to access Molpro registries

---
+++ pymolpro/registry.py
@@ -0,0 +1,73 @@
+import atexit
+import shutil
+import tempfile
+import pathlib
+
+from pymolpro import Project
+
+_registry_project = None
+_registry_project_directory = None
+
+
+def ensure_registry_project():
+    global _registry_project
+    global _registry_project_directory
+    if _registry_project is None:
+        _registry_project_directory = tempfile.mkdtemp()
+        _registry_project = Project((pathlib.Path(_registry_project_directory) / 'registry_project').as_posix())
+        atexit.register(_registry_project_delete)
+
+def _registry_project_delete():
+    shutil.rmtree(_registry_project_directory, ignore_errors=True)
+
+def local_molpro_root():
+    r"""
+    Get the directory of the Molpro installation in the local backend
+
+    :return: directory
+    :rtype: pathlib.Path
+    """
+    ensure_registry_project()
+    return _registry_project.local_molpro_root
+
+
+def procedures_registry():
+    r"""
+    Get the procedures registry from the Molpro pointed to in the local backend
+
+    :return:
+    :rtype: dict
+    """
+    ensure_registry_project()
+    return _registry_project.procedures_registry()
+
+
+def basis_registry():
+    r"""
+    Get the basis registry from the Molpro pointed to in the local backend
+
+    :return:
+    :rtype: dict
+    """
+    ensure_registry_project()
+    return _registry_project.basis_registry()
+
+
+def registry(set=None):
+    """
+    Access the registry belonging to Molpro in the `local` backend.
+
+    :param set: If present, obtain as a dictionary the specified registry set. If absent, obtain a list of all sets
+    :return:
+    """
+    ensure_registry_project()
+    return _registry_project.registry(set)
+
+
+def allowed_methods():
+    result = []
+    _procedures_registry = procedures_registry()
+    for keyfound in _procedures_registry.keys():
+        if _procedures_registry[keyfound]['class'] == 'PROG':
+            result.append(_procedures_registry[keyfound]['name'])
+    return result
---
+++ pymolpro/test_registry.py
@@ -0,0 +1,43 @@
+import subprocess
+import unittest
+
+import pytest
+
+import pymolpro
+import os
+import shutil
+
+
+class TestRegistry(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_procedures_registry(self):
+        proc_reg = pymolpro.procedures_registry()
+        if proc_reg:
+            assert proc_reg['PNO-UCCSD']['gradient'] == -1
+            assert 'pairopt' in proc_reg['PNO-UCCSD']['options']
+
+    def test_basis_registry(self):
+        basis_reg = pymolpro.basis_registry()
+        assert None not in basis_reg.keys()
+        if basis_reg:
+            assert basis_reg['cc-pV(D+d)Z']['quality'] == 'DZ'
+
+    def test_molpro_root(self):
+        # print('registry',pymolpro.run_local_molpro(['--registry']).stdout)
+        if pymolpro.local_molpro_root():
+            assert os.path.exists(pymolpro.local_molpro_root() / 'lib' / 'defbas')
+
+    def test_registry(self):
+        if pymolpro.registry():
+            assert 'GMB' in pymolpro.registry()
+            assert 'STATES' in pymolpro.registry('GMB')
+            assert pymolpro.registry('GMB')['STATES']['set'] == 'GMB'
+
+
+if __name__ == '__main__':
+    unittest.main()
