bd1f720a6d4f1f6e876d737137dd4a1b102e7c3d
Peter Knowles <KnowlesPJ@Cardiff.ac.uk>
2026-05-26 12:24:30 +0100

    Accelerate evaluation of orbitals on a grid by trimming regions where value is below a threshold

--- pymolpro/cube_data.py
+++ pymolpro/cube_data.py
@@ -2,13 +2,14 @@ import numpy as np
 
 
 class CubeData:
-    def __init__(self, source: str|dict = None):
+    def __init__(self, source: str | dict = None):
         if source is None:
             pass
         elif type(source) == str and source.endswith('.cube'):
             self.load_from_cube_file(source)
         elif isinstance(source, dict):
-            for attr in ['title', 'natoms', 'origin', 'orbitals', 'nval', 'dimensions', 'cells', 'atoms', 'data','orbital_identifiers']:
+            for attr in ['title', 'natoms', 'origin', 'orbitals', 'nval', 'dimensions', 'cells', 'atoms', 'data',
+                         'orbital_identifiers']:
                 if attr in source:
                     setattr(self, attr, source[attr])
         else:
@@ -54,6 +55,7 @@ class CubeData:
                         k0 += len(line)
                         if k0 >= self.dimensions[2]:
                             break
+
     def __str__(self):
         buffer = self.title[0] + '\n' + self.title[1] + '\n'
         buffer += f'{self.natoms * (-1 if self.orbitals else 1)} {self.origin[0]} {self.origin[1]} {self.origin[2]} 1\n'
@@ -71,7 +73,7 @@ class CubeData:
             buffer += f'{len(self.orbital_identifiers)}'
             for identifier in self.orbital_identifiers:
                 try:
-                    buffer += f' {int(float(identifier)*10)}'
+                    buffer += f' {int(float(identifier) * 10)}'
                 except ValueError:
                     buffer += f' 1'
             buffer += '\n'
@@ -79,9 +81,41 @@ class CubeData:
                 for j in range(self.dimensions[1]):
                     k0 = 0
                     while k0 < self.dimensions[2]:
-                        for k in range(min(6,self.dimensions[2] - k0)):
-                            buffer += f' {self.data[i, j, k0]:.5e}'.replace('e','E')
+                        for k in range(min(6, self.dimensions[2] - k0)):
+                            buffer += f' {self.data[i, j, k0]:.5e}'.replace('e', 'E')
                             k0 += 1
                         buffer += '\n'
 
         return buffer
+
+    def __len__(self):
+        return self.dimensions[0] * self.dimensions[1] * self.dimensions[2]
+
+    def bounds(self, threshold: float = 1e-4):
+        if not isinstance(threshold, float):
+            return None
+        for i0 in range(self.dimensions[0]):
+            if np.any(self.data[i0, :, :] >= threshold):
+                break
+        for i1 in reversed(range(self.dimensions[0])):
+            if np.any(self.data[i1, :, :] >= threshold):
+                break
+        for j0 in range(self.dimensions[1]):
+            if np.any(self.data[:, j0, :] >= threshold):
+                break
+        for j1 in reversed(range(self.dimensions[1])):
+            if np.any(self.data[:, j1, :] >= threshold):
+                break
+        for k0 in range(self.dimensions[2]):
+            if np.any(self.data[:, :, k0] >= threshold):
+                break
+        for k1 in reversed(range(self.dimensions[2])):
+            if np.any(self.data[:, :, k1] >= threshold):
+                break
+        # print(self.dimensions)
+        # print(i0, i1, j0, j1, k0, k1)
+        return (
+            (self.origin[0] + (i0-1) * self.cells[0, 0], self.origin[0] + (i1+1) * self.cells[0, 0]),
+            (self.origin[1] + (j0-1) * self.cells[1, 1], self.origin[1] + (j1+1) * self.cells[1, 1]),
+            (self.origin[2] + (k0-1) * self.cells[2, 2], self.origin[2] + (k1+1) * self.cells[2, 2])
+        )
